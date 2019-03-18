14fec972937fcaf79780863f962767bcafb5ba93
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-03-18 21:28:55 +0300

    added test demo implementation of play sound functional to sound manager

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<AudioClip> AudioClips = new List<AudioClip>();
+
+    private AudioSource CreateAudioSourceWithAudioClip(string name, float volume, bool loop)
     {
-        
+        var audioClip = AudioClips.FirstOrDefault(x => x.name == name);
+        if (audioClip == null)
+        {
+            Debug.LogError("No audioClip was found with specified name :" + name);
+            return null;
+        }
+
+        var gameObject = new GameObject();
+        var audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        audioSource.loop = loop;
+
+        return audioSource;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySound2D(string name, float volume, bool loop)
     {
-        
+        CreateAudioSourceWithAudioClip(name, volume, loop);
+    }
+
+    public void PlaySound3D(string name, float volume, bool loop, GameObject bindGameObject, bool fallowGameObject)
+    {
+        var audioSource = CreateAudioSourceWithAudioClip(name, volume, loop);
+
+        audioSource.gameObject.transform.position = bindGameObject.transform.position;
+        if (fallowGameObject)
+        {
+            audioSource.gameObject.transform.SetParent(bindGameObject.transform);
+        }
     }
 }
