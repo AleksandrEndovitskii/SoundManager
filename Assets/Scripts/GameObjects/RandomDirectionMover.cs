49bc86547d8450d63e1a225d06d303718ad40b15
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-03-19 18:14:49 +0300

    added randomly moved object

---
+++ Assets/Scripts/GameObjects/RandomDirectionMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace GameObjects
+{
+    public class RandomDirectionMover : MonoBehaviour
+    {
+        [SerializeField]
+        private float Speed = 1f;
+
+        private Vector3 _direction;
+
+        private void Start()
+        {
+            var randomX = Random.Range(-1.0f, 1.0f);
+            var randomZ = Random.Range(-1.0f, 1.0f);
+            var randomDirection = (new Vector3(randomX, 0.0f, randomZ)).normalized;
+            _direction = randomDirection;
+        }
+
+        private void Update()
+        {
+            transform.position += _direction * Speed * Time.deltaTime;
+        }
+    }
+}
