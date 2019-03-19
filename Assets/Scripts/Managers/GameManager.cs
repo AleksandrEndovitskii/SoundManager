f0bdf8399b67a367a4e94f702a5541807f506116
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-03-19 18:20:16 +0300

    added game object manager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,13 +6,17 @@ namespace Managers
     {
         [SerializeField]
         private SoundManager soundManagerPrefab;
+        [SerializeField]
+        private GameObjectManager gameObjectManagerPrefab;
 
         private SoundManager _soundManagerInstance;
+        private GameObjectManager _gameObjectManagerInstance;
 
         // Start is called before the first frame update
         private void Start()
         {
             _soundManagerInstance = Instantiate(soundManagerPrefab);
+            _gameObjectManagerInstance = Instantiate(gameObjectManagerPrefab);
         }
 
         // Update is called once per frame
---
+++ Assets/Scripts/Managers/GameObjectManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Managers
+{
+    public class GameObjectManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject gameObjectPrefab;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
