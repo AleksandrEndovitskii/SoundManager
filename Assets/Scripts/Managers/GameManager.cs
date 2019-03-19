008aae2abf0c00e4520f078c2c67fde0ca306b60
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-03-19 18:02:57 +0300

    added instantiating of sound manager on application start

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,13 +1,18 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace Managers
 {
     public class GameManager : MonoBehaviour
     {
+        [SerializeField]
+        private SoundManager soundManagerPrefab;
+
+        private SoundManager _soundManagerInstance;
+
         // Start is called before the first frame update
         void Start()
         {
-        
+            _soundManagerInstance = Instantiate(soundManagerPrefab);
         }
 
         // Update is called once per frame
