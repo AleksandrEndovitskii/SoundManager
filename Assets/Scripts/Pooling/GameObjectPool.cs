a7d6bc01ec51dc6da95b359a053759a235707e4e
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-04-03 16:36:15 +0300

    added empty demo implementation of Pool

---
+++ Assets/Scripts/Pooling/GameObjectPool.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Pooling
+{
+    public class GameObjectPool : MonoBehaviour
+    {
+        [SerializeField]
+        public GameObject gameObjectPrefab;
+
+        // в начале создаёться пулл определённого размера
+        // в процессе работы если места мало - увеличить
+        //                   если места много - уменьшить
+        [SerializeField]
+        public int capacity;
+    }
+}
---
+++ Assets/Scripts/Pooling/Pool.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Pooling
+{
+    public static class Pool
+    {
+        public static GameObject Spawn(GameObject prefab)
+        {
+            return Object.Instantiate(prefab);
+        }
+
+        public static T Spawn<T>(T prefab)
+            where T : Component
+        {
+            return Object.Instantiate(prefab);
+        }
+
+        public static void Despawn(GameObject clone)
+        {
+            Object.Destroy(clone);
+        }
+    }
+}
